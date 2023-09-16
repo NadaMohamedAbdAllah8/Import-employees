d70920dc5c2c95905ec80b42aeb9a1c3ccc7fb03
nada <nadamohamedabdallah8@gmail.com>
2023-09-16 20:37:58 +0300

    feat: Viewing more employee relations

--- app/Http/Resources/Admin/EmployeeResource.php
+++ app/Http/Resources/Admin/EmployeeResource.php
@@ -14,6 +14,26 @@ class EmployeeResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'first_name' => $this->first_name,
+            'middle_initial' => $this->middle_initial,
+            'last_name' => $this->last_name,
+            'username' => $this->user_name,
+            'gender' => $this->gender,
+            'email' => $this->email,
+            'date_of_birth' => $this->date_of_birth,
+            'time_of_birth' => $this->time_of_birth,
+            'age_in_years' => $this->age_in_years,
+            'phone_number' => $this->phone_number,
+            'place_name' => $this->place_name,
+            'date_of_joining' => $this->date_of_joining,
+            'age_in_company_in_years' => $this->age_in_company_in_years,
+            'prefix' => $this->prefix ? $this->prefix->prefix : null,
+            'zip_code_id' => $this->zipCode ? $this->zipCode->code : null,
+            'city' => $this->city ? $this->city->name : null,
+            'county' => $this->county ? $this->county->name : null,
+            'region' => $this->region ? $this->region->name : null,
+        ];
     }
 }
