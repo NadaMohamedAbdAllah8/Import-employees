0a8df38587427ddf6081cce76bed8bece5ba3178
nada <nadamohamedabdallah8@gmail.com>
2023-09-16 20:37:43 +0300

    feat: Defining more relations in the Employee model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -2,10 +2,92 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'user_name',
+        'first_name',
+        'last_name',
+        'middle_initial',
+        'gender',
+        'email',
+        'date_of_birth',
+        'time_of_birth',
+        'age_in_years',
+        'phone_number',
+        'place_name',
+        'date_of_joining',
+        'age_in_company_in_years',
+        'prefix_id',
+        'zip_code_id',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d',
+    ];
+
+    public function getTimeOfBirthAttribute($value)
+    {
+        return Carbon::createFromFormat('H:i:s', $value)->format('h:i:s A');
+    }
+
+    public function prefix(): BelongsTo
+    {
+        return $this->belongsTo(Prefix::class, 'prefix_id');
+    }
+
+    public function zipCode(): BelongsTo
+    {
+        return $this->belongsTo(ZipCode::class, 'zip_code_id');
+    }
+
+    public function city(): HasOneThrough
+    {
+        return $this->hasOneThrough(
+            City::class,
+            ZipCode::class,
+            'id',
+            'id',
+            'zip_code_id',
+            'city_id'
+        );
+    }
+
+    public function county(): HasOneThrough
+    {
+        return $this->hasOneThrough(
+            County::class,
+            City::class,
+            'id',
+            'id',
+            'zip_code_id',
+            'county_id'
+        );
+    }
+
+    public function region(): HasOneThrough
+    {
+        return $this->hasOneThrough(
+            Region::class,
+            County::class,
+            'id',
+            'id',
+            'zip_code_id',
+            'region_id'
+        );
+    }
 }
