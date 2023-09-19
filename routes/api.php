f439ed480b9ed1a411397b1893c31c9d079ef890
nada <nadamohamedabdallah8@gmail.com>
2023-09-19 21:36:50 +0300

    refactor: Removing route prefix

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
  */
 Route::post('/employee', [EmployeeController::class, 'import']);
 
-Route::group(['prefix' => 'admin', 'namespace' => 'App\Http\Controllers\Admin'],
+Route::group(['namespace' => 'App\Http\Controllers\Admin'],
     function () {
         Route::post('login', [AuthController::class, 'login']);
 
@@ -28,3 +28,35 @@ Route::group(['prefix' => 'admin', 'namespace' => 'App\Http\Controllers\Admin'],
         });
 
     });
+/*
+$curl = curl_init();
+
+$file_path = 'D:\Programming-Studying\Laravel\Projects\Import-employees\import-employees-totally-new\importCopy.csv'; // Make sure to specify the correct path to your CSV file
+
+$post_data = file_get_contents($file_path);
+
+curl_setopt_array($curl, array(
+CURLOPT_URL => "http://127.0.0.1:8000/api/employee",
+CURLOPT_RETURNTRANSFER => true,
+CURLOPT_ENCODING => "",
+CURLOPT_MAXREDIRS => 10,
+CURLOPT_TIMEOUT => 30000,
+CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+CURLOPT_CUSTOMREQUEST => "POST",
+CURLOPT_POSTFIELDS => $post_data,
+CURLOPT_HTTPHEADER => array(
+'Content-Type: text/csv',
+),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+echo "cURL Error #:" . $err;
+} else {
+print_r(json_decode($response));
+}
+ */
