bbf2dd6dfedc4460a949ca749c1de3bfe345155f
nada <nadamohamedabdallah8@gmail.com>
2024-02-14 19:53:56 +0200

    test: Country update and delete

--- tests/Feature/CountyTest.php
+++ tests/Feature/CountyTest.php
@@ -14,10 +14,10 @@ class CountyTest extends CustomTestCase
     public function setUp(): void
     {
         parent::setUp();
-        $this->route = $this->base_route . 'counties';
+        $this->route = $this->base_route . 'counties/';
     }
 
-    public function test_store_successfully()
+    public function test_store_county_successfully()
     {
         // arrange
         $county = County::factory()->make();
@@ -36,5 +36,50 @@ class CountyTest extends CustomTestCase
         $response->assertCreated()
             ->assertJsonPath('item.name', $county->name)
             ->assertJsonPath('item.region.id', $county->region->id);
+    }
+
+    public function test_update_county_successfully()
+    {
+        // arrange
+        $old_county =  County::factory()->create();
+
+        $county = County::factory()->make();
+
+        $admin = Admin::factory()->create();
+
+        $response = $this->actingAs($admin, 'api')
+            ->patchJson($this->route . $old_county->id, $county->toArray());
+
+        // assert
+        $this->assertDatabaseHas(County::class, [
+            'id' => $old_county->id,
+            'name' => $county->name,
+            'region_id' => $county->region_id
+        ]);
+
+        $response->assertSuccessful()
+            ->assertJsonPath('item.id', $old_county->id)
+            ->assertJsonPath('item.name', $county->name)
+            ->assertJsonPath('item.region.id', $county->region->id);
+    }
+
+    public function test_delete_county_successfully()
+    {
+        // arrange
+        $county = County::factory()->create();
+
+        $admin = Admin::factory()->create();
+
+        $response = $this->actingAs($admin, 'api')
+            ->deleteJson($this->route . $county->id);
+
+        // assert
+        $this->assertDatabaseMissing(County::class, [
+            'id' => $county->id,
+            'name' => $county->name,
+            'region_id' => $county->region_id
+        ]);
+
+        $response->assertNoContent();
     }
 }
